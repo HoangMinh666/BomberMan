074c23c251dd5ded8e89116df26e3c44b16919ea
HoangMinh666 <111828077+HoangMinh666@users.noreply.github.com>
2022-08-29 17:13:51 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,5 +1,7 @@
 #include "Game.h"
-#include <string>
+#include "UI.h"
+
+char ch;
 
 GameBomberMan::GameBomberMan()
 {
@@ -22,54 +24,74 @@ GameBomberMan::~GameBomberMan()
 	delete monster;
 }
 
-void GameBomberMan::OnePlayerMode(UI* ui)
+void GameBomberMan::OnePlayerMode(UI *ui)
 {
 	system("CLS");
 	bool finish = false;
 
-	map->DisplayMap(); player1->Display();
-	GoToXY(0, MAX_HEIGHT);  cout << "Remaining lives: " << player1->_liveLeft;
-	while (1)
+	while (ui->window->isOpen()) 
 	{
-		if (_kbhit())
-		{
-			char ch = _getch();
-			if (ch == 72 || ch == 80 || ch == 75 || ch == 77) player1->Move(map, ch, bomb1);
-			else if (ch == ' ') player1->SetBomb(bomb1, map);
-			else if (ch == 'p') char ctmp = _getch();
-		}
+		Event e;
 
-		player1->Display();
+		while (ui->window->pollEvent(e))
+		{
+			if (e.type == Event::Closed)
+				ui->window->close();
+
+			ch = 0;
+			if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Left) ch=75;
+			else if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Right) ch=77;
+			else if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Up) ch=72;
+			else if(e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Down) ch=80;
+			else if (e.type == sf::Event::KeyPressed && e.key.code == sf::Keyboard::Space) ch = 57;
+
+			//if (Keyboard::isKeyPressed(Keyboard::Left)) ch = 75;
+			//if (Keyboard::isKeyPressed(Keyboard::Right)) ch = 77;
+			//if (Keyboard::isKeyPressed(Keyboard::Up)) ch = 72;
+			//if (Keyboard::isKeyPressed(Keyboard::Down)) ch = 80;
+
+			if (ch == 72 || ch == 80 || ch == 75 || ch == 77) player1->Move(map, ch, bomb1);
+			else if (ch == 57 ) player1->SetBomb(bomb1, map);
+			//else if (ch == 'p') _getch();
+
+
+		}
+		monster->Move(map);
 		player1->GetPortal(map, bomb1, portal);
 		if (!bomb1->_isExplosion)
 		{
 			if (bomb1->BombExplosion(map, portal, brick, &player1->_bomberMan, monster))
-			{
 				player1->ResetBomberMan(bomb1);
-				GoToXY(17, MAX_HEIGHT);	cout << player1->_liveLeft;
-			}
 		}
 		if (player1->CollideMonster(monster))
 		{
-			player1->ResetBomberMan(bomb1);
-			GoToXY(17, MAX_HEIGHT);	cout << player1->_liveLeft;
+			player1->ResetBomberMan(bomb1); GoToXY(17, MAX_HEIGHT);
+			cout << player1->_liveLeft << endl;
+
 		}
 
-		monster->Move(map);
-		monster->Display();
 
 		if (brick->IsDestroyed() || !player1->_liveLeft || monster->IsDeadAll())
 		{
 			finish = true;
 			break;
 		}
+		//update UI
+		ui->window->clear();
+		ui->DisplayMap(map); 
+		monster->DisplayMonsterUI(*ui->spriteMonster, *ui->window);	
+		player1->DisplayBomberManUI(*ui->spriteBomberMan, *ui->window);
+		ui->window->display();
+
+		GoToXY(0, MAX_HEIGHT);  cout << "Remaining lives: " << player1->_liveLeft;
+
 	}
 	system("CLS");
 	if (brick->IsDestroyed() || monster->IsDeadAll()) cout << "CHUC MUNG BAN DA CHIEN THANG";
 	else if (player1->_liveLeft == 0) cout << "GAME OVER";
 }
 
-void GameBomberMan::TwoPlayersMode()
+void GameBomberMan::TwoPlayersMode(UI *ui)
 {
 	system("CLS");
 
@@ -82,18 +104,35 @@ void GameBomberMan::TwoPlayersMode()
 	GoToXY(0, MAX_HEIGHT + 2);  cout << "Remaining lives of player 2: " << player2->_liveLeft;
 	GoToXY(0, MAX_HEIGHT + 3);  cout << "Score of player 2: " << scorePlayer2;
 
-	while (1)
+	while (ui->window->isOpen()) 
 	{
-		if (_kbhit())
-		{
-			char ch = _getch();
-			if (ch == 72 || ch == 80 || ch == 75 || ch == 77) player2->Move(map, ch, bomb2);
-			else if (ch == 'w' || ch == 'a' || ch == 's' || ch == 'd') player1->Move(map, ch, bomb1);
-			else if (ch == 13) player2->SetBomb(bomb2, map);
-			else if (ch == ' ') player1->SetBomb(bomb1, map);
-			else if (ch == 'p') char ctmp = _getch();
-		}
+		Event e1;
 
+		while(ui->window->pollEvent(e1))
+		{
+			if (e1.type == Event::Closed)
+				ui->window->close();
+
+			ch = 0;
+			if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::A) ch='a';
+			else if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::D) ch='d';
+			else if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::W) ch='w';
+			else if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::S) ch='s';
+			else if (e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::Space) ch = ' ';
+			if (ch == 'a' || ch == 'd' || ch == 'w' || ch == 's') player1->Move(map, ch, bomb1);
+			else if (ch == ' ' ) player1->SetBomb(bomb1,map);
+
+			if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::Left) ch=75;
+			else if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::Right) ch=77;
+			else if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::Up) ch=72;
+			else if(e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::Down) ch=80;
+			else if (e1.type == sf::Event::KeyPressed && e1.key.code == sf::Keyboard::Return) ch = 58;
+			if (ch == 72 || ch == 80 || ch == 75 || ch == 77) player2->Move(map, ch, bomb2);
+			else if (ch == 58 ) player2->SetBomb(bomb2, map);
+
+		
+		}
+		monster->Move(map);
 		if (!player1->CheckIsDead())
 		{
 			player1->Display();
@@ -146,8 +185,14 @@ void GameBomberMan::TwoPlayersMode()
 				GoToXY(29, MAX_HEIGHT + 2);	cout << player2->_liveLeft;
 			}
 		}
-		monster->Move(map);
-		monster->Display();
+
+		//update UI
+		ui->window->clear();
+		ui->DisplayMap(map); 
+		monster->DisplayMonsterUI(*ui->spriteMonster, *ui->window);	
+		player1->DisplayBomberManUI(*ui->spriteBomberMan, *ui->window);
+		player2->DisplayBomberManUI(*ui->spriteBomberMan2, *ui->window);
+		ui->window->display();
 
 		if (brick->IsDestroyed() || monster->IsDeadAll() || (player2->CheckIsDead() && player1->CheckIsDead()))
 		{
